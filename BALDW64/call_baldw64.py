31da416c7f05d599780ea870388df6eb8a7d78e6
Avik RC <roychoudhuryavik005@gmail.com>
2025-05-28 14:11:40 +0530

    Add files via upload

---
+++ BALDW64/call_baldw64.py
@@ -0,0 +1,20 @@
+from src.main import baldw64
+
+passwd = 'password'
+email = 'testuser@gmail.com'
+
+reg_data = baldw64(message=passwd.encode(), verf_str=email)
+
+message = input("Enter your passwd: ")
+verf_str = input("Enter your email for verification: ")
+hashed_message = baldw64(message=message.encode(), verf_str=verf_str)
+
+hash_size = reg_data[:4]
+size = int(hash_size, 16)
+
+if hashed_message[4:size // 8] == reg_data[4:size // 8]:
+    print("Welcome, user!")
+    print(reg_data)
+    print(hashed_message)
+else:
+    print("Incorrect data!")
